358f82f26b1fcabf659ac6634e2af1c9febdbd54
jmediane <jawad.mediane.ext@veolia.com>
2024-08-26 16:42:47 +0100

    Mise en place et lecture du fichier json

---
+++ main.py
@@ -0,0 +1,15 @@
+import json 
+
+# Chargement des pokemons : json => python list
+with open("pokemons.json", "r") as file :
+    pokemon_list = json.load( file )
+
+# Tri de la liste par "id" (facultatif) : 
+pokemon_list = sorted( pokemon_list, key=lambda x: x['id'], reverse=False )
+
+# list => dict : indexé par "id"
+pokemon_dict = {
+    k+1 : v  for k,v in enumerate(pokemon_list)
+}
+
+print(pokemon_dict)
